c2dcdb33d3d797e67316d366a9220ae37fd54cce
fenekku <fenekku@fenekku.com>
2013-12-30 15:17:19 -0500

    Miscellaneous fixes and additions;
    * Single quotes are only valid for characters. Characters
      include some escape sequences defined here:
      http://nimrod-lang.org/manual.html#string-literals
    * Newlines between patterns for better readability
    * Upper and lower case i,u
    * Raw strings: double quoted strings prefixed by r do not highlight
      escape sequences differently
    * Triple quoted strings: those strings are raw by definition
    * Better organized syntaxTest.nim

--- syntaxTest.nim
+++ syntaxTest.nim
@@ -6,8 +6,35 @@
 import tables
 from sets import TSet
 
+var a : string = "Hello\n Wo\r\cld!\l\f" & "\t\v \\ \"I\'\4h \am\b h\ere\x FF."
+var aa : cstring = r"Hello\n Wo\r\cld!\l\f" #raw string
+var b : int = 10
+var bb : cint = 10
+var c : seq[int]
+var d : tuple[hello : string, world : int]
+var e : bool = true or false
+var g : char = 'd'
+# Invalid characters are highlighted in pink!
+var i : char = 'Good riddance Mr. Charles \n' & 'Good day Mrs. Shultz'
+var ii = '\n'
+var iii = '\t'
+var iiii = '\xAA'
+var j : float = 54.34543
+
+var
+  k : int = 0o1'i8
+  l : float = 3.0
+  m : uint8 = 0x12'u8
+
+const
+  global_constant* = 3'i8
+
+type
+  TType = object
+  PType = ref TType
+
 proc toLower(c: char): char = # toLower for characters
-  if c in {'A'..'Z'}:
+  if c in {'a'..'Z'}:
     result = chr(ord(c) + (ord('a') - ord('A')))
   else:
     result = c
@@ -27,46 +54,11 @@ converter toBool*(a: Bool32): bool = bool(a)
 macro parseOptions*(body: stmt): stmt {.immediate.}=
   body.expectKind nnkStmtList
 
-var a : string = "Hello World!" & " I am here."
-var aa : string = "Hello\n Wo\r\cld!\l\f" & "\t\v \\ \"I\'\4h \am\b h\ere\x FF."
-var b : int = 10
-var c : seq[int]
-var d : tuple[hello : string, world : int]
-var e : bool = true
-var f : bool = false
-var g : char = 'd'
-var h : cint = 10
-var i : cstring = 'Good riddance Mr. Charles \n' & 'Good day Mrs. Shultz'
-var j : float = 54.34543
-
-var
-  k : int = 0o1'i8
-  l : float = 3.0
-  m :uint8 = 0x12'u8
-
-
-const
-  GLFW_VERSION_MAJOR* = 3'i8
-  GLFW_VERSION_MINOR* = 0'i8
-  GLFW_VERSION_REVISION* = 1'i8
-
-
-type
-  TTyoe = object
-  PType = ref TType
-  TBla = object
-
 case k
 of 1:
-  echo("Hello")
   return "Camera .position=[$# $# $#]" % map(camera.position, floatToStr)
 of 2:
-  echo("Bla")
-  return [0'u, 0'u32, 0x30'u8,0x00'u8,0x10'u8,0xFF'u8]
-
-# type
-#   TType = object
-#   PType = ref TType
+  return [0'u, 0'u32, 0x30'u8, 0x00'u8, 0x10'u8,0xFF'u8]
 
 proc `$`(a : int): string =
   return $a
@@ -79,16 +71,8 @@ if a >= b and b <= c and f:
 else:
   return b
 
-if window == nil:
-  glfwTerminate()
-
-h = 0.0
-
 if 1 in 0..4:
   echo "here"
-
-proc setLineLength*(layout: PLayout; len: cfloat) {.
-  importc: "ftglSetLayoutLineLength", dynlib: LibName.}
 
 let
   a = @[1, 2, 3, 4]
@@ -97,12 +81,6 @@ assert b == @["1", "2", "3", "4"]
 
 method selectup*(screen : PScreen) =
   echo("Pressed Up Arrow")
-
-method exit*(screen : PScreen) =
-  quit "cyao!"
-
-proc foo(a : string) =
-  a.toUpperCase()
 
 try:
   # Main keyboard loop.
@@ -114,9 +92,15 @@ except:
   var msg = getCurrentExceptionMsg()
   debug_log "Terminated by signal " & $msg
 
-var inTemplateModule = true
 template b(a : expr): stmt =
   bind inTemplateModule
 
 block:
-  echo "I am in a block"
+  echo "I am in a block\n"
+
+echo """I am a long piece of text
+        That takes multiple lines.\n
+     """
+
+echo r"""I am a raw long piece of text but that doesn't matter\n"""
+
